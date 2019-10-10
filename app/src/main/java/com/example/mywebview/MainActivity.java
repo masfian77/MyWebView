87c94af2ac9a6e49292890ca82ad50c6b3c5a4a5
ALFIAN SATRIA V.P <masfian77@gmail.com>
2019-10-10 12:16:42 +0700

    committed

---
+++ app/src/main/java/com/example/mywebview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mywebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webView);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Toast.makeText(MainActivity.this, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final android.webkit.JsResult result) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                result.confirm();
+                return true;
+            }
+        });
+
+        myWebView.loadUrl("https://www.dicoding.com");
+    }
+}
